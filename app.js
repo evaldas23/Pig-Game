31c75b54c4976f2db82c8ed92c431313bdb90b49
Evaldas <evaldas.cerniauskas@gmail.com>
2019-03-18 12:20:29 +0200

    add false statement when is winner

--- app.js
+++ app.js
@@ -1,88 +1,77 @@
-
-
-var scores, roundScore, activePlayer, gamePlaying;
-
+var scores, roundScores, actyvePlayer, gamePlaying;
 init();
 
-
-document.querySelector('.btn-roll').addEventListener('click', function () {
+document.querySelector('.btn-roll').addEventListener('click', function() {
   if (gamePlaying) {
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1;
 
-    //2. Display the result
+    // 2. Display the resut
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
 
-
-    //3. Update the round score IF the rolled number was NOT a 1
+    // 3. Update the round score IF  the rolled number was NOT a 1
     if (dice !== 1) {
-      //Add score
+      // Prideti rezultata
       roundScore += dice;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-      //Next player
+      // kitas zaidejas
       nextPlayer();
     }
   }
 });
 
-
-document.querySelector('.btn-hold').addEventListener('click', function () {
+document.querySelector('.btn-hold').addEventListener('click', function() {
   if (gamePlaying) {
-    // Add CURRENT score to GLOBAL score
+    //prideti esama rezultata i globalu rezultata
     scores[activePlayer] += roundScore;
 
-    // Update the UI
+    //atnaujint (UI) vartotojo sasaja
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    // Check if player won the game
-    if (scores[activePlayer] >= 100) {
-      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    //Patikrinti ar zaidejas laimejo
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!!!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       gamePlaying = false;
-    } else {
-      //Next player
+    }
+    //Next player
+    else {
       nextPlayer();
     }
   }
 });
 
-
 function nextPlayer() {
-  //Next player
-  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
   roundScore = 0;
-
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
 
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
 
-  //document.querySelector('.player-0-panel').classList.remove('active');
-  //document.querySelector('.player-1-panel').classList.add('active');
-
   document.querySelector('.dice').style.display = 'none';
 }
 
-document.querySelector('.btn-new').addEventListener('click', init);
+document.querySelector('.btn-new').addEventListener('click', init); //panaikinau anonimos funkcija todel kad nereiketu kviesti viena funkcija kad iskviestum kita, ir '()' neuzdejau prie init todel kad neiskviestu iskarto sios funkcijos o kviestu kai paspaudi mygtyka.
 
 function init() {
   scores = [0, 0];
-  activePlayer = 0;
   roundScore = 0;
+  activePlayer = 0;
   gamePlaying = true;
 
   document.querySelector('.dice').style.display = 'none';
 
-  document.getElementById('score-0').textContent = '0';
-  document.getElementById('score-1').textContent = '0';
-  document.getElementById('current-0').textContent = '0';
-  document.getElementById('current-1').textContent = '0';
+  document.getElementById('score-0').textContent = 0;
+  document.getElementById('score-1').textContent = 0;
+  document.getElementById('current-0').textContent = 0;
+  document.getElementById('current-1').textContent = 0;
   document.getElementById('name-0').textContent = 'Player 1';
   document.getElementById('name-1').textContent = 'Player 2';
   document.querySelector('.player-0-panel').classList.remove('winner');
@@ -91,4 +80,3 @@ function init() {
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
 }
-
